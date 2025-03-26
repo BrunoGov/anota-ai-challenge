69792b95222f763cae0e51eba6964d7af50c1cab
Bruno Morais <bmorais794@gmail.com>
2025-03-26 18:10:16 -0300

    feat: added default route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
-import { Routes } from '@angular/router';
-import { HomeComponent } from './pages/home/home.component';
+import { Routes } from "@angular/router";
+import { HomeComponent } from "./pages/home/home.component";
 
 export const routes: Routes = [
   {
-    path: 'home',
+    path: "",
+    redirectTo: "home",
+    pathMatch: "full",
+  },
+  {
+    path: "home",
     component: HomeComponent,
   },
 ];
